3d7b2e301e8d180700f4ee62870129c648eb07cc
LiNk-NY <mramos09@gmail.com>
2020-02-11 12:20:55 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# The suggested name for this image is: bioconductor/bioconductor_docker:devel
+FROM bioconductor/bioconductor_docker:devel
+
+LABEL name="waldronlab/sratool" \
+      version="0.99.1" \
+      url="https://github.com/waldronlab/sratool" \
+      vendor="Waldron Lab" \
+      maintainer="marcel.ramos@sph.cuny.edu" \
+      description="SRAtool for downloading and processing data on the cloud" \
+      license="Artistic-2.0"
+
+RUN mkdir -p $HOME/ncbi/public $HOME/src \
+    && cd $HOME/src \
+    && wget "http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.10.2/sratoolkit.2.10.2-ubuntu64.tar.gz" \
+    && tar -xzf sratoolkit.2.10.2-ubuntu64.tar.gz
+
---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+biocIMG=bioconductor/bioconductor_docker:devel
+
+docker pull $biocIMG
+
+DOCKER_RPKGS="$HOME/.docker-$version-packages"
+DOCKER_HOME="$HOME/dockerhome"
+R_LIBS='/usr/local/lib/R/host-site-library:/usr/local/lib/R/site-library:/usr/local/lib/R/library'
+
+mkdir -p "$DOCKER_RPKGS"
+mkdir -p "$DOCKER_HOME"
+
+if [ ! -f "$DOCKER_HOME"/.Renviron ]; then
+    echo "R_LIBS=$R_LIBS" > $DOCKER_HOME/.Renviron
+fi
+
+TOKEN_EXP='GITHUB_[P|T].*'
+
+GPAT=`grep "$TOKEN_EXP" ~/.Renviron`
+GRENV=`grep "$TOKEN_EXP" $DOCKER_HOME/.Renviron`
+
+if [ ! -z "${GPAT// }" ] && [ -z "${GRENV// }" ]; then
+    echo $GPAT >> $DOCKER_HOME/.Renviron
+fi
+
+echo "Installed packages will go in host directory: $DOCKER_RPKGS"
+# echo "RStudio home directory will be mounted on host directory: $DOCKER_HOME"
+
+docker image build -t waldronlab/sratool:latest .
+
+docker run -ti --user rstudio \
+    -v $DOCKER_HOME:/home/rstudio \
+    -v $DOCKER_RPKGS:/usr/local/lib/R/host-site-library \
+    -w /home/rstudio \
+    waldronlab/sratool:latest bash
+
